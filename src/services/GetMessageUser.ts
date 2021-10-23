cbc5c26354d6ce279b146e110996fd4082996e17
Guss-droid <gustavo.re@inforoute.com.br>
2021-10-23 02:16:44 -0300

    ajuste final

--- src/services/GetMessageUser.ts
+++ src/services/GetMessageUser.ts
@@ -4,11 +4,14 @@ export class GetMessagesUser {
 
   async execute(id_user: string) {
     const messages = await prismaClient.message.findMany({
-      take: 10,
+      take: 5,
       where: {
         user_id: id_user
       },
-      include: { user: true }
+      include: { user: true },
+      orderBy: {
+        created_at: "desc"
+      }
     })
 
     return messages
